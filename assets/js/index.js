3b2bbc5b17f045cc3f8e054de17dd55120592c3c
Otvas33 <jeancharlesotvas@gmail.com>
2025-07-18 16:24:17 +0200

    commitJs

---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //voici mon commentaire
+
+// const lait = 20;
+// const beurre = 35;
+// let total = 0;
+
+// total = lait + beurre;
+// console.log(total);
+
+// const chocolatine = 20;
+// const painAuchoco = 30;
+// let total = 0;
+
+// total = chocolatine + painAuchoco;
+// console.log(total);
+
+
+//exo 1
+
+const name = 'jeancharles';
+console.log(name);
+alert('jeancharles');
+
+//exo 2
+
+let age = 32;
+let message = " i am " + name + " et j'ai " + age;
+console.log(message);
+
+// exo 3
+
+const ageOnemore = age + 1;
+let ageMessage = "l'année prochaine j'aurais " + ageOnemore;
+console.log(ageMessage);
+
+
+// exo 4
+let toto = "toto";
+let tata = "tata";
+let invers = tata;
+tata = toto;
+toto = invers;
+console.log("dans toto:" + toto, "dans tata:" + tata);
